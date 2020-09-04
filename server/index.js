481ec8cb1e4b6613e1dc819a8acdd5a6e3ddfe55
Giang-Le <giangle0411@gmail.com>
2020-09-04 21:44:43 +1000

    Node/Express, Setup server for auth

---
+++ server/index.js
@@ -0,0 +1,23 @@
+// Main starting point of the application
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+const router = require('./router')
+
+// App Setup
+// Middlewares - Any incoming request will be passed to
+// morgan and bodyParser
+// Morgan is a logging framework, logging incoming request (used for debugging)
+// bodyParser parse the request into json
+
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+router(app)
+
+// Server Setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+console.log('Server listening on:', port)
