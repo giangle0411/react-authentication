73a713306acec5fb98471c2249a79356392ddf57
Giang-Le <giangle0411@gmail.com>
2020-09-08 16:00:01 +1000

    passport authentication strategies, login

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-### The project is made for Practicing Authorization
+# The project is made for Practicing Authorization
 
-## This project contains server end and client end
+### This project contains server end and client end
 
-# Technologies used:
+## Server
+
+An authentication API Server
+
+### Technologies used:
 
 - express js
 - MongoDB
 - React
+
+### some of the installed packages:
+
+- `bcrypt-nodejs`: used in user model to encrypt user saved password
+- `jwt-simple`: used to generate json web token, so when a user makes an auth request, they should include their JWT
+- `passport`: authentication library for node and express, used for cookie based authentication, create a jwt strategy that attempts to authenticate a user given a JSON web token
+- `passport-local`: used to create a local strategy that attempts to authenticate a user given only an email and a password
--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,19 @@
+const jwt = require('jwt-simple')
 const User = require('../models/user')
+const config = require('../config')
+
+function tokenForUser(user) {
+  // sub: subject (who does this token belong to)
+  // iat: issue at time
+  const timestamp = new Date().getTime()
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
+
+exports.signin = function (req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) })
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email
@@ -31,7 +46,16 @@ exports.signup = function (req, res, next) {
         return next(err)
       }
       // Respond to request indicating the user was created
-      res.json({ success: true })
+      // Save generated token
+      res.json({ token: tokenForUser(user) })
     })
   })
 }
+
+// When signing up or signing in, give a token inexchange for an id
+// User ID + Our secret string = JSON Web Token
+
+// In the future, when a user makes an auth request, they should include their JWT
+// JSON Web Token + Our secret string = User ID
+
+// More on JWT, visit jwt.io
